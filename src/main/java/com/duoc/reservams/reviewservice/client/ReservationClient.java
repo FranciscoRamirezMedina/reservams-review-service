307219033b7631210bccfa1074ee18fc5b4fdac7
Francisco Ramírez <TUEMAIL@gmail.com>
2026-06-26 20:12:51 -0400

    fix: use service discovery for reservation client

--- src/main/java/com/duoc/reservams/reviewservice/client/ReservationClient.java
+++ src/main/java/com/duoc/reservams/reviewservice/client/ReservationClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 // cliente Feign para comunicarse con reservation-service
-@FeignClient(name = "reservams-reservation-service", url = "http://localhost:8086")
+@FeignClient(name = "reservams-reservation-service")
 public interface ReservationClient {
 
     // busca una reserva por ID en reservation-service
